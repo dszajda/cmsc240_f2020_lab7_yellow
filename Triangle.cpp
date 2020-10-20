0a79ac8330f70c5114bd5b822214ddf2ba84406b
al3kq <al3kq@mathcs02.richmond.edu>
2020-10-20 17:10:57 -0400

    Changed header spelling

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,23 +1,21 @@
-#ifndef __TRAINGLE_CPP__
-#define __TRAINGLE_CPP__
+#ifndef __TRIANGLE_CPP__
+#define __TRIANGLE_CPP__
 
 #include "Triangle.h"
 #include "Shape.h"
 
-Triangle::Triangle(std::string name, double base, double height) :triangleName(name), base(base), height(height)
-{
-	Shape(name);
-}
+using namespace std;
+
+Triangle::Triangle(string name, double base, double height) : Shape(name), base{base}, height{height} {}
 
 Triangle::~Triangle(){}
 
 double Triangle::getArea(){
 	return (height * base)/2;
-
 }
 
 void Triangle::print(){
-	Shape::print()
+	Shape::print();
 	cout << "base: " << base << endl;
 	cout << "height: " << height << endl;
 }
--- Triangle.h
+++ Triangle.h
@@ -1,18 +1,19 @@
 #include "Shape.h"
 #ifndef __TRIANGLE_H__
-#define __TRAINGLE_H__
+#define __TRIANGLE_H__
 
 #include <string>
+
+using namespace std;
 
 class Triangle : public Shape
 {
 	private:
-		std::string name;
 		double base;
 		double height;
 
 	public:
-		Triangle(std::string triangleName, double base, double height);
+		Triangle(string triangleName, double base, double height);
 		~Triangle();
 		
 		double getArea();
