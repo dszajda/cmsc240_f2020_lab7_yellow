dc04822b6a441d56dbfd055d93b263d0bf84375f
al3kq <al3kq@mathcs02.richmond.edu>
2020-10-20 15:45:05 -0400

    Updated Shape.cpp

---
+++ Shape.cpp
@@ -0,0 +1,15 @@
+#ifndef __Shape_CPP__
+#define __Shape_CPP__
+
+Shape::Shape(std::string shapeName) :name(shapeName)  {}
+Shape::~Shape() {}
+
+virtual double getArea(){
+	return 0.0;
+}
+
+virtual void print(){
+	cout << "name: " << name << endl;
+}
+
+#endif
